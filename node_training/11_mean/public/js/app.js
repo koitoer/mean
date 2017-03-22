0d706c23002ecc27dcd68ecb4247818b79000301
mauricio mena <mmena@homeaway.com>
2017-03-21 23:32:35 -0600

    Add module 11 and 12

---
+++ node_training/11_mean/public/js/app.js
@@ -0,0 +1,16 @@
+angular.module('TestApp', []);
+
+angular.module('TestApp')
+	.controller('MainController', ctrlFunc);
+
+function ctrlFunc(){
+	this.message = 'Hello';
+
+	this.people = [
+		{name : 'Koitoer'},
+		{name : 'Koito'},
+		{name : 'Per'}
+	]
+
+	this.people = clientPeople;
+}
